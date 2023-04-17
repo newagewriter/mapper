8af146b47a2bca5ff08faed77b5ec314e7c5b80d
newagewriter <39876191+newagewriter@users.noreply.github.com>
2023-04-17 22:35:24 +0200

    Prepare 0.3.0-rc. Change dependencies for json library; use json-simp… (#49)
    * Prepare 0.3.0-rc. Change dependencies for json library; use json-simple fork
    
    * Add documentation. Code clining

--- lib/src/main/java/io/github/newagewriter/processor/converter/PrimitiveConverter.kt
+++ lib/src/main/java/io/github/newagewriter/processor/converter/PrimitiveConverter.kt
@@ -3,7 +3,53 @@ package io.github.newagewriter.processor.converter
 import java.math.BigDecimal
 import java.math.BigInteger
 
+/**
+ * PrimitiveConverter instance is a object which helps to make conversion between primitive type
+ */
 object PrimitiveConverter {
+
+    /**
+     * Cast primitive type given type.
+     * @param element - value to cast
+     * @param type - type to be cast
+     * @return object of given type or exception if cast cannot be made
+     */
+    fun<U> castPrimitiveTo(element: Any, type: Class<U>): U {
+        return when (type.simpleName) {
+            Int::class.java.simpleName -> toInt(element)
+            Byte::class.java.simpleName -> toByte(element)
+            Short::class.java.simpleName -> toShort(element)
+            Float::class.java.simpleName -> toFloat(element)
+            Char::class.java.simpleName -> toChar(element)
+            else -> element
+        } as U
+    }
+
+    /**
+     * Method check if given class is primitive type or string
+     * @return true for all primitive type and string, false otherwise
+     */
+    @JvmStatic
+    fun isPrimitive(type: Class<*>): Boolean {
+        return when (type.simpleName) {
+            String::class.java.simpleName,
+            Char::class.java.simpleName,
+            Boolean::class.java.simpleName,
+            Byte::class.java.simpleName,
+            Short::class.java.simpleName,
+            Int::class.java.simpleName,
+            Long::class.java.simpleName,
+            Float::class.java.simpleName,
+            Double::class.java.simpleName -> true
+            else -> false
+        }
+    }
+
+    /**
+     * Method convert primitive type to Char
+     * @param element - element to convert
+     * @return Char value or exception if cast cannot be made
+     */
     fun toChar(element: Any): Char {
         return when (element) {
             is Character -> element.charValue()
@@ -14,6 +60,12 @@ object PrimitiveConverter {
             else -> throw java.lang.ClassCastException("Cannot cast ${element.javaClass.simpleName} to Char")
         }
     }
+
+    /**
+     * Method convert primitive type to Float
+     * @param element - element to convert
+     * @return Float value or exception if cast cannot be made
+     */
     fun toFloat(element: Any): Float {
         return when (element) {
             is Double -> element.toFloat()
@@ -24,6 +76,11 @@ object PrimitiveConverter {
         }
     }
 
+    /**
+     * Method convert primitive type to Short
+     * @param element - element to convert
+     * @return Short value or exception if cast cannot be made
+     */
     fun toShort(element: Any): Short {
         return when (element) {
             is Int -> element.toShort()
@@ -34,6 +91,11 @@ object PrimitiveConverter {
         }
     }
 
+    /**
+     * Method convert primitive type to Byte
+     * @param element - element to convert
+     * @return Byte value or exception if cast cannot be made
+     */
     fun toByte(element: Any): Byte {
         return when (element) {
             is Int -> element.toByte()
@@ -45,6 +107,11 @@ object PrimitiveConverter {
         }
     }
 
+    /**
+     * Method convert primitive type to Int
+     * @param element - element to convert
+     * @return Int value or exception if cast cannot be made
+     */
     fun toInt(element: Any): Int {
         return when (element) {
             is Long -> element.toInt()
